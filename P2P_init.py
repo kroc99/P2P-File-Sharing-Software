a3b20dfc328699122137f5f58547a6e32080beff
Savannah Ogletree <bff8078@gmail.com>
2025-10-16 17:54:07 -0400

    able to read the peers info from PeerInfo, this allows for us to now work with variables

--- P2P_init.py
+++ P2P_init.py
@@ -4,6 +4,7 @@ import socket
 import os
 import sys
 
+# Global variables
 with open('Common.cfg', 'r') as file:
     for line in file:
         if line.startswith('NumberOfPreferredNeighbors'):
@@ -25,4 +26,16 @@ with open('Common.cfg', 'r') as file:
             PIECE_SIZE = int(line.split(' ')[1].strip())
             print(f"PieceSize: {PIECE_SIZE}")
 
-#with open('PeerInfo.cfg', 'r') as file:
+with open('PeerInfo.cfg', 'r') as file: # peerinfo is like a tracker equivalent in BitTorrent
+    peer_info = {}
+    for line in file:
+        parts = line.split(' ')
+        peer_id = int(parts[0].strip())
+        host_name = parts[1].strip()
+        port_number = int(parts[2].strip())
+        has_file = parts[3].strip() == '1'
+        peer_info[peer_id] = (host_name, port_number, has_file)
+
+    #how to access: peer_info[1001][0]
+
+#need a main or header file and src file but here is a couple functions
