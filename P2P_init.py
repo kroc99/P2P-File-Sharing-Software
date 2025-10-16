c61ef09deb170a65938e2263bbd1938c7779e6cb
Savannah Ogletree <bff8078@gmail.com>
2025-10-16 17:46:10 -0400

    able to read in the common.cfg file for initialization

--- P2P_init.py
+++ P2P_init.py
@@ -6,4 +6,23 @@ import sys
 
 with open('Common.cfg', 'r') as file:
     for line in file:
-        
+        if line.startswith('NumberOfPreferredNeighbors'):
+            NUMBER_OF_PREFERRED_NEIGHBORS = int(line.split(' ')[1].strip())
+            print(f"NumberOfPreferredNeighbors: {NUMBER_OF_PREFERRED_NEIGHBORS}")
+        elif line.startswith('UnchokingInterval'):
+            UNCHOKING_INTERVAL = int(line.split(' ')[1].strip())
+            print(f"UnchokingInterval: {UNCHOKING_INTERVAL}")
+        elif line.startswith('OptimisticUnchokingInterval'):
+            OPTIMISTIC_UNCHOKING_INTERVAL = int(line.split(' ')[1].strip())
+            print(f"OptimisticUnchokingInterval: {OPTIMISTIC_UNCHOKING_INTERVAL}") 
+        elif line.startswith('FileName'):  
+            FILE_NAME = line.split(' ')[1].strip()
+            print(f"FileName: {FILE_NAME}")
+        elif line.startswith('FileSize'):
+            FILE_SIZE = int(line.split(' ')[1].strip())
+            print(f"FileSize: {FILE_SIZE}")
+        elif line.startswith('PieceSize'):
+            PIECE_SIZE = int(line.split(' ')[1].strip())
+            print(f"PieceSize: {PIECE_SIZE}")
+
+#with open('PeerInfo.cfg', 'r') as file:
